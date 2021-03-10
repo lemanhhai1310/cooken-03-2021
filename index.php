0d0426bd5ed8488143f0413b3ce3b48ce14ec830
HAI LE MANH <hai.lemanh.1310@gmail.com>
2021-03-10 15:21:51 +0700

    REFERRAL PROGRAM + export _index.html

--- index.php
+++ index.php
@@ -2,6 +2,23 @@
 <?php $is_home = true; ?>
 <?php require "template-parts/layouts/header.php"; ?>
 
+<!--REFERRAL PROGRAM-->
+<div class="home__section03 mb-120-m uk-flex uk-flex-middle uk-text-center uk-background-norepeat uk-background-center-center" uk-height-viewport="offset-bottom: true" data-src="images/bg3.png" uk-img>
+    <div class="uk-section uk-width-1-1">
+        <div class="uk-container uk-container-small">
+            <div class="uk-flex-center" uk-grid>
+                <div class="uk-width-2-5@m">
+                    <div class="uk-text-center uk-margin home__boxCommon mb-37-m">
+                        <h2 class="uk-h2 uk-text-uppercase home__boxCommon__title">REFERRAL PROGRAM</h2>
+                    </div>
+                    <p class="home__section01__box1__txt uk-margin-remove">We appreciate it when you introduce us to new clients. By way of thank you, we will credit you with 5% of future revenues made by publishers you send our way. Statistics and referral links are available in your account. Help us help your contacts help you!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/REFERRAL PROGRAM-->
+
 <!--ĐĂNG KÝ TƯ VẤN-->
 <?php require "template-parts/layouts/section02.php"; ?>
 <!--/ĐĂNG KÝ TƯ VẤN-->
