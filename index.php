b42f711ee44760b184b933f8a4f1e0dc7886c64c
HAI LE MANH <hai.lemanh.1310@gmail.com>
2021-03-10 11:12:24 +0700

    footer + lien he

--- index.php
+++ index.php
@@ -2,4 +2,10 @@
 <?php $is_home = true; ?>
 <?php require "template-parts/layouts/header.php"; ?>
 
+<!--LIÊN HỆ,HÌNH THỨC THANH TOÁN-->
+<div class="uk-section uk-height-viewport uk-background-norepeat uk-background-top-center" data-src="images/bg1.png" uk-img>
+
+</div>
+<!--/LIÊN HỆ,HÌNH THỨC THANH TOÁN-->
+
 <?php require "template-parts/layouts/footer.php"; ?>
--- template-parts/layouts/footer.php
+++ template-parts/layouts/footer.php
@@ -1,3 +1,61 @@
+<footer class="footer">
+    <div class="uk-section-small footer__top uk-light">
+        <div class="uk-container">
+            <div class="uk-child-width-1-4@m uk-margin footer__top__grid1" uk-grid>
+                <div>
+                    <h5 class="uk-h5 footer__top__title">Về chúng tôi</h5>
+                    <h5 class="uk-h5 footer__top__title">Dự án</h5>
+                </div>
+                <div>
+                    <h5 class="uk-h5 footer__top__title">Dịch vụ</h5>
+                    <ul class="uk-list footer__top__menu">
+                        <li><a href="">Facebook Marketing</a></li>
+                        <li><a href="">Tiktok Marketing</a></li>
+                        <li><a href="">Application Marketing</a></li>
+                        <li><a href="">Media Solutions</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h5 class="uk-h5 footer__top__title">Liên hệ</h5>
+                    <ul class="uk-list footer__top__menu">
+                        <li><a href="">FAQ</a></li>
+                        <li><a href="">Live Chat</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h5 class="uk-h5 footer__top__title">Chính sách</h5>
+                    <ul class="uk-list footer__top__menu">
+                        <li><a href="">Quy trình làm việc</a></li>
+                        <li><a href="">Chính sách thanh toán</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="footer__top__box1">
+                <div>© 2021 Cassandera Capital LTD</div>
+                <div>Toà nhà HNC Premier - số 8 Lý Nam 1 </div>
+            </div>
+        </div>
+    </div>
+    <div class="uk-section-xsmall footer__bottom">
+        <div class="uk-container">
+            <div class="uk-child-width-auto uk-grid-small uk-grid-20-m uk-flex-center" uk-grid>
+                <?php
+                $data = array(
+                    'Tư vấn dịch vụ',
+                    'Dự án thành công',
+                    'Gọi lại cho tôi',
+                    'Khách hàng tiêu biểu',
+                );
+                foreach ($data as $k => $v): ?>
+                <div>
+                    <a href="" class="<?= ($k == 0) ? 'uk-active' : '' ?> footer__bottom__btn uk-button uk-button-secondary uk-border-rounded"><?= $v ?></a>
+                </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </div>
+</footer>
 </section>
 </body>
 </html>
