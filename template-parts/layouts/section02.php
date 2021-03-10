584e29f7cf0db908dfbc69ef246dedddb7c612e0
HAI LE MANH <hai.lemanh.1310@gmail.com>
2021-03-10 14:30:57 +0700

    ĐĂNG KÝ TƯ VẤN + export _index.html

---
+++ template-parts/layouts/section02.php
@@ -0,0 +1,32 @@
+<div class="home__section02 uk-section uk-background-norepeat uk-background-top-center" uk-height-viewport="offset-bottom: true" data-src="images/bg2.png" uk-img>
+    <div class="uk-container uk-container-small">
+        <div class="uk-text-center uk-margin home__boxCommon mb-40-m">
+            <h2 class="uk-h2 uk-text-uppercase home__boxCommon__title">ĐĂNG KÝ TƯ VẤN</h2>
+            <p class="uk-margin-remove home__boxCommon__desc">Hãy để Cooken Agency đồng hành cùng thành công của bạn</p>
+        </div>
+        <div class="uk-flex-center" uk-grid>
+            <div class="uk-width-3-5@m">
+                <form class="uk-grid-small uk-grid-20-m uk-margin mb-40-m" uk-grid>
+                    <div class="uk-width-1-1">
+                        <input class="uk-input uk-border-rounded home__section02__input" type="text" placeholder="Email">
+                    </div>
+                    <div class="uk-width-1-1">
+                        <input class="uk-input uk-border-rounded home__section02__input" type="text" placeholder="Tên">
+                    </div>
+                    <div class="uk-width-1-2">
+                        <input class="uk-input uk-border-rounded home__section02__input" type="text" placeholder="Số điện thoại">
+                    </div>
+                    <div class="uk-width-1-2">
+                        <input class="uk-input uk-border-rounded home__section02__input" type="text" placeholder="Lĩnh vực">
+                    </div>
+                    <div class="uk-width-1-1">
+                        <input class="uk-input uk-border-rounded home__section02__input" type="text" placeholder="Ngân sách dự kiến">
+                    </div>
+                </form>
+                <div class="uk-text-center">
+                    <button type="button" class="home__section02__btn uk-button uk-button-secondary uk-border-pill">NHẬN ĐĂNG KÝ TƯ VẤN</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
